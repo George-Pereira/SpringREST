7914f198120b3cb7ebea59b549d29530db84b578
George-Pereira <fernanr8@gmail.com>
2021-01-17 13:32:37 -0300

    Configuração do Travis CI para monitoramento de deployments do projeto

---
+++ src/main/java/com/fernando/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.fernando.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig 
+{
+	private static final String TITLE = "Livraria";
+	private static final String DESC = "API profissional para Livrarias";
+	private static final String VERSION = "V1";
+	private static final String DEV = "Fernando George Pereira";
+	private static final String SITE = "https://www.github.com/George-Pereira";
+	private static final String CONTACT = "George.fpereira06@gmail.com";
+	@Bean
+	public Docket api() 
+	{
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.fernando")).paths(PathSelectors.any()).build().apiInfo(buildApiInfo());
+	}
+
+	private ApiInfo buildApiInfo() 
+	{
+		return new ApiInfoBuilder().title(TITLE).description(DESC).version(VERSION).contact(new Contact(DEV, SITE, CONTACT)).build();
+	}
+}
--- src/main/java/com/fernando/controller/Livrocontroller.java
+++ src/main/java/com/fernando/controller/Livrocontroller.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/v1/livros")
+
 public class Livrocontroller 
 {
 	@GetMapping
+	@ApiOperation(value = "Retorna uma string por meio da API")
+	@ApiResponses(value = {
+							@ApiResponse(code = 200, message = "Operação efetuada com Sucesso!")
+	})
 	public String hello() 
 	{
 		return "Olá Mundo";
