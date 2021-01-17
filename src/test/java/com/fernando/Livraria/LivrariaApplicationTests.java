fc06f40b5fe5714b5ad6d608ef237078166a88b2
George-Pereira <fernanr8@gmail.com>
2021-01-17 14:24:21 -0300

    Alterações Travis CI

--- src/test/java/com/fernando/Livraria/LivrariaApplicationTests.java
+++ src/test/java/com/fernando/Livraria/LivrariaApplicationTests.java
@@ -1,9 +1,11 @@
 package com.fernando.Livraria;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication
 class LivrariaApplicationTests {
 
 	@Test
