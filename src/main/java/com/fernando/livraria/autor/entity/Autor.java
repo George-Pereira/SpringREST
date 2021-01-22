b5fe0aa0f8eaf3e5e3393fb715dc492af5404d3b
George-Pereira <fernanr8@gmail.com>
2021-01-22 14:29:05 -0300

    Adição de uma classe auditoravel para entidades

--- src/main/java/com/fernando/livraria/autor/entity/Autor.java
+++ src/main/java/com/fernando/livraria/autor/entity/Autor.java
@@ -10,13 +10,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fernando.livraria.entity.Auditoria;
 import com.fernando.livraria.livro.entity.Livro;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class Autor 
+public class Autor extends Auditoria
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
